8d0ec57f29ddbeb6e4d60f9ece9efdf938415d7a
Dima Lavrov <aston007d@yandex.ru>
2022-11-07 20:32:13 +0600

    Добавьте файлы проекта.

---
+++ WpfAppItebTab_Left/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppItebTab_Left
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void addTabItemFigur_Click(object sender, RoutedEventArgs e)
+        {
+            int count = tabControlFigur.Items.Count;
+            count++;
+            TabItem newItem = new TabItem();
+            newItem.Header = $"Шаблон{count}";
+            //Заполняем контент
+            newItem.Content = "text";
+            tabControlFigur.Items.Add(newItem);
+        }
+    }
+}
